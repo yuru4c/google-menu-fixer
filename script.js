cc63f9f6397f2af9b1e2730c19d716da72da3b81
ゆる <yuru4c@gmail.com>
2020-02-06 09:07:37 +0900

    v4.1.1
    リファクタリング

--- script.js
+++ script.js
@@ -1,126 +1,111 @@
 (function ($, _) {
 
 var prefix = 'gmf-';
-var qName = 'q qs';
 
 _.runtime.sendMessage('get', function (prefs) {
 	var order  = prefs.order;
 	var length = prefs.length;
 	var hide   = prefs.hide;
 	
-	function Item(q) {
-		this.href = q.href;
-		this.children = q.childNodes;
-		var index = order.indexOf(q.textContent);
+	function Item(a) {
+		this.href = a.href;
+		this.children = a.childNodes;
+		var index = order.indexOf(a.textContent);
 		this.index = index == -1 ? order.length : index;
 	}
-	Item.compare = function (a, b) {
-		return a.index - b.index;
-	};
-	function createQ(item) {
-		var a = $.createElement('a');
-		a.className = qName;
-		a.href = item.href;
-		while (item.children.length) {
-			a.appendChild(item.children[0]);
-		}
-		return a;
-	}
-	
-	function createA(item, className) {
+	Item.prototype.createA = function (className) {
 		var a = $.createElement('a');
 		a.className = className;
-		a.href = item.href;
-		while (item.children.length) {
-			a.appendChild(item.children[0]);
+		a.href = this.href;
+		while (this.children.length) {
+			a.appendChild(this.children[0]);
 		}
 		return a;
+	};
+	
+	function Sort(sel, as) {
+		var selItem = new Item(sel);
+		var items = [selItem];
+		for (var i = 0, l = as.length; i < l; i++) {
+			items.push(new Item(as[i]));
+		}
+		items.sort(function (a, b) {
+			return a.index - b.index;
+		});
+		
+		var index = items.indexOf(selItem);
+		if (index >= length) {
+			items.splice(index, 1);
+			index = length - 1;
+			items.splice(index, 0, selItem);
+		}
+		
+		this.items = items;
+		this.index = index;
+		this.length = length > items.length ?
+			items.length : length;
 	}
 	
 	function newer(){
 		var vis = $.getElementsByClassName('T47uwc')[0];
-		var last = vis.lastChild;
 		var sel = vis.getElementsByClassName('rQEFy')[0];
-		var qs = $.querySelectorAll('[jsname="ONH4Gc"]');
-		var i, l = qs.length;
-		var more = qs[l - 1].parentNode;
+		var last = vis.lastChild;
+		var as = vis.querySelectorAll('[jsname="ONH4Gc"]');
+		var more = as[as.length - 1].parentNode;
 		
-		var selItem = new Item(sel);
-		var items = [selItem];
-		for (i = 0; i < l; i++) items.push(new Item(qs[i]));
-		items.sort(Item.compare);
+		var sort = new Sort(sel, as);
+		var items = sort.items;
+		var i;
 		
-		var selIndex = items.indexOf(selItem);
-		if (selIndex >= length) {
-			items.splice(selIndex, 1);
-			selIndex = length - 1;
-			items.splice(selIndex, 0, selItem);
+		sel.remove();
+		for (i = 0; i < as.length; i++) {
+			as[i].remove();
 		}
-		
-		sel.parentNode.removeChild(sel);
-		for (i = 0; i < l; i++) {
-			var r = qs[i];
-			r.parentNode.removeChild(r);
-		}
-		
-		l = items.length;
-		if (length > l) length = l;
 		
 		var a;
-		for (i = 0; i < length; i++) {
-			if (i == selIndex) {
+		for (i = 0; i < sort.length; i++) {
+			if (i == sort.index) {
 				vis.insertBefore(sel, last);
 				continue;
 			}
-			a = createA(items[i], 'NZmxZe');
+			a = items[i].createA('NZmxZe');
 			vis.insertBefore(a, last);
 		}
-		for (; i < l; i++) {
-			a = createA(items[i], 'cF4V5c-ibnC6b');
+		for (; i < items.length; i++) {
+			a = items[i].createA('cF4V5c-ibnC6b');
 			more.appendChild(a);
 		}
-		if (l == length) {
+		if (items.length == sort.length) {
 			more.previousSibling.style.display = 'none';
 		}
 	}
 	function older(vis){
+		var qName = 'q qs', moreName = qName + ' f9UGee';
 		var sel = vis.getElementsByClassName('hdtb-msel')[0];
 		var qs = vis.parentNode.getElementsByClassName(qName);
-		var i, l = qs.length;
-		var more = qs[l - 1].parentNode;
+		var more = qs[qs.length - 1].parentNode;
 		
-		var selItem = new Item(sel);
-		var items = [selItem];
-		for (i = 0; i < l; i++) items.push(new Item(qs[i]));
-		items.sort(Item.compare);
-		
-		var selIndex = items.indexOf(selItem);
-		if (selIndex >= length) {
-			items.splice(selIndex, 1);
-			selIndex = length - 1;
-			items.splice(selIndex, 0, selItem);
-		}
-		
-		l = items.length;
-		if (length > l) length = l;
+		var sort = new Sort(sel, qs);
+		var items = sort.items;
+		var i;
 		
 		vis.innerHTML = more.innerHTML = '';
-		for (i = 0; i < length; i++) {
-			if (i == selIndex) {
+		
+		for (i = 0; i < sort.length; i++) {
+			if (i == sort.index) {
 				vis.appendChild(sel);
 				continue;
 			}
 			var div = $.createElement('div');
 			div.className = 'hdtb-mitem hdtb-imb';
-			div.appendChild(createQ(items[i]));
+			div.appendChild(items[i].createA(qName));
 			vis.appendChild(div);
 		}
-		for (; i < l; i++) {
-			var q = createQ(items[i]);
-			q.className += ' f9UGee';
+		for (; i < items.length; i++) {
+			var q = items[i].createA(moreName);
 			more.appendChild(q);
 		}
-		if (l == length) {
+		if (items.length == sort.length) {
 			more.previousSibling.style.display = 'none';
 		}
 	}
